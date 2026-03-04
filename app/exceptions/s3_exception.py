4cbb8cd3424bb777072f80e78c1c353648de2365
Parakram <parakram@keemail.me>
2026-03-04 16:43:17 +0545

    Custom Exceptions

---
+++ app/exceptions/s3_exception.py
@@ -0,0 +1,36 @@
+class S3Exception(Exception):
+    """Base exception for S3-related errors."""
+
+    pass
+
+
+class S3UploadException(S3Exception):
+    """Exception raised when an error occurs during S3 upload."""
+
+    def __init__(self, message: str = "Failed to upload file to S3"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class S3DownloadException(S3Exception):
+    """Exception raised when an error occurs during S3 download."""
+
+    def __init__(self, message: str = "Failed to download file from S3"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class S3DeleteException(S3Exception):
+    """Exception raised when an error occurs during S3 delete."""
+
+    def __init__(self, message: str = "Failed to delete file from S3"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class S3ConnectionException(S3Exception):
+    """Exception raised when an error occurs during S3 connection."""
+
+    def __init__(self, message: str = "Failed to connect to S3"):
+        self.message = message
+        super().__init__(self.message)
