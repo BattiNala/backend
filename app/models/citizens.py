43d0bb882be9d13cabab5041d0b3ac9f270bd742
Parakram <parakram@keemail.me>
2026-03-04 15:38:51 +0545

    Citizen Model and Migrations
    Co-authored-by:  Ankit Diyal <ankitdiyal17@gmail.com>

--- app/models/citizens.py
+++ app/models/citizens.py
@@ -10,7 +10,7 @@ class Citizen(Base):
     name = Column(String(100), nullable=False)
     email = Column(String(100), nullable=True, unique=True)
     phone_number = Column(String(20), nullable=True, unique=True)
-    home_address = Column(String(200), nullable=True)
+    home_address = Column(String(200), nullable=False)
     trust_score = Column(Integer, default=0)
 
     user_id = Column(
@@ -23,11 +23,11 @@ class Citizen(Base):
 
     user = relationship("User", back_populates="citizen_profile", uselist=False)
 
-    reported_issues = relationship(
-        "Issue",
-        back_populates="reporter",
-        foreign_keys="Issue.reporter_id",
-    )
+    # reported_issues = relationship(
+    #     "Issue",
+    #     back_populates="reporter",
+    #     foreign_keys="Issue.reporter_id",
+    # )
 
     def __repr__(self):
         return f"<Citizen(citizen_id={self.citizen_id}, name='{self.name}', email='{self.email}', phone_number='{self.phone_number}', home_address='{self.home_address}', trust_score={self.trust_score})>"
