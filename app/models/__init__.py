e937ee1631c49a39ab01789ef5eb3d7d6d25ce86
Parakram <parakram@keemail.me>
2026-03-05 18:40:35 +0545

    missing alembic importer

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+from .citizens import Citizen
+from .department import Department
+from .roles import Role
+from .user import User
+from .team import Team
+from .employee import Employee
