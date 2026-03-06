2533259fab81668437f64e3b412b2f6979a5a50a
Parakram <parakram@keemail.me>
2026-03-06 17:43:33 +0545

    feat: function to check resend available

--- app/utils/otp_utils.py
+++ app/utils/otp_utils.py
@@ -1,9 +1,21 @@
 import secrets
 import hmac
 import hashlib
+from datetime import datetime, timezone
 
 
 class OtpUtils:
+    @staticmethod
+    def handle_otp_generation(otp_length: int = 6) -> tuple[str, bytes, bytes]:
+        """
+        Handle OTP generation with input validation,salt gen and hash.
+        """
+
+        otp_code = OtpUtils.generate_otp(otp_length)
+        otp_salt = OtpUtils.generate_salt()
+        otp_hash = OtpUtils.hash_otp(otp_code, otp_salt)
+        return otp_code, otp_salt, otp_hash
+
     @staticmethod
     def generate_otp(length: int = 6) -> str:
         """
@@ -41,6 +53,8 @@ class OtpUtils:
         """
         Verify OTP using constant-time comparison.
         """
+        if OtpUtils.is_otp_expired(stored_hash):
+            return False
         candidate_hash = OtpUtils.hash_otp(otp_input, salt)
         return hmac.compare_digest(candidate_hash, stored_hash)
 
@@ -49,6 +63,17 @@ class OtpUtils:
         """
         Check if the OTP is expired based on the current time and the expires_at timestamp.
         """
-        from datetime import datetime, timezone
 
         return datetime.now(timezone.utc) > expires_at
+
+    @staticmethod
+    def is_resend_available(resend_available_at) -> tuple[bool, int]:
+        """
+        Check if the OTP resend is available based on the current time and the resend_available_at timestamp.
+        """
+        time_remaining = 0
+
+        available = datetime.now(timezone.utc) >= resend_available_at
+        if not available:
+            time_remaining = (resend_available_at - datetime.now(timezone.utc)).seconds
+        return available, time_remaining
