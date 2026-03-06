87ab2f47ef7287bd06db6c4ed7d34ba097a2d5ea
Parakram <parakram@keemail.me>
2026-03-06 15:38:05 +0545

    OTP utility functions

---
+++ app/utils/time.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+
+def utc_to_timezone(utc_datetime, timezone="Asia/Kathmandu"):
+    """Convert UTC datetime to specified timezone."""
+    if isinstance(utc_datetime, str):
+        utc_datetime = datetime.fromisoformat(utc_datetime)
+
+    utc_datetime = utc_datetime.replace(tzinfo=ZoneInfo("UTC"))
+    return utc_datetime.astimezone(ZoneInfo(timezone))
