d08bc3ab08d38962d283793f341647b16a6eba79
Parakram <parakram@keemail.me>
2026-03-07 13:29:40 +0545

    reorder listing in swagger

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,11 +1,12 @@
 """Main API v1 router registration."""
 
 from fastapi import APIRouter
-from app.api.v1.endpoints import issues, routes, auth, superadmin
+from app.api.v1.endpoints import issues, routes, auth, superadmin, role
 
 api_router = APIRouter()
 
+api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(role.router, prefix="/role", tags=["role"])
 api_router.include_router(issues.router, prefix="/issues", tags=["issues"])
 api_router.include_router(routes.router, prefix="/routes", tags=["routes"])
-api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(superadmin.router, prefix="/superadmin", tags=["superadmin"])
