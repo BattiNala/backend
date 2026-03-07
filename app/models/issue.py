f2146d43718fd40feb44f0bff06676654891d2a6
Parakram <parakram@keemail.me>
2026-03-07 16:33:05 +0545

    use department name as issue type instead of enum to handle more gracefully

--- app/models/issue.py
+++ app/models/issue.py
@@ -3,18 +3,19 @@
 # pylint: disable=not-callable
 
 from sqlalchemy import (
+    Boolean,
     Column,
+    DateTime,
+    Enum as SQLEnum,
+    ForeignKey,
     Integer,
     String,
-    Enum as SQLEnum,
-    DateTime,
-    ForeignKey,
     func,
 )
 from sqlalchemy.orm import relationship
-from app.db.base import Base
 
-from app.schemas.issue import IssueType, IssueStatus
+from app.db.base import Base
+from app.schemas.issue import IssueStatus
 
 
 class Issue(Base):  # pylint: disable=too-few-public-methods
@@ -23,10 +24,15 @@ class Issue(Base):  # pylint: disable=too-few-public-methods
     __tablename__ = "issues"
 
     issue_id = Column(Integer, primary_key=True, autoincrement=True)
-    issue_type = Column(SQLEnum(IssueType, name="issue_type_enum"), nullable=False)
+    issue_type = Column(
+        Integer,
+        ForeignKey("departments.department_id", ondelete="RESTRICT"),
+        nullable=False,
+        index=True,
+    )
     description = Column(String(1000), nullable=False)
     status = Column(SQLEnum(IssueStatus, name="issue_status_enum"), nullable=False)
-    is_anonymous = Column(bool, default=False, nullable=False)
+    is_anonymous = Column(Boolean, default=False, nullable=False)
     reporter_id = Column(
         Integer,
         ForeignKey("citizens.citizen_id", ondelete="RESTRICT"),
@@ -41,9 +47,7 @@ class Issue(Base):  # pylint: disable=too-few-public-methods
         index=True,
     )
 
-    created_at = Column(
-        DateTime(timezone=True), server_default=func.now(), nullable=False
-    )
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = Column(
         DateTime(timezone=True),
         server_default=func.now(),
@@ -51,12 +55,11 @@ class Issue(Base):  # pylint: disable=too-few-public-methods
         nullable=False,
     )
 
-    reporter = relationship(
-        "Citizen", back_populates="reported_issues", foreign_keys=[reporter_id]
-    )
+    reporter = relationship("Citizen", back_populates="reported_issues", foreign_keys=[reporter_id])
     assignee = relationship(
         "Employee", back_populates="assigned_issues", foreign_keys=[assignee_id]
     )
+    department = relationship("Department", back_populates="issues", foreign_keys=[issue_type])
     # attachments = relationship(
     #     "Attachment", back_populates="issue", cascade="all, delete-orphan"
     # )
