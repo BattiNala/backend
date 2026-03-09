c5a922f8c0cea2c88c3adb8d8f9290556ed76303
Parakram <89983841+ParakramK@users.noreply.github.com>
2026-03-09 13:00:17 +0545

    add Citizen profile endpoint (#7)

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -2,11 +2,12 @@
 
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import auth, department, issues, role, routes, superadmin
+from app.api.v1.endpoints import auth, department, issues, profile, role, routes, superadmin
 
 api_router = APIRouter()
 
 api_router.include_router(auth.auth_router, prefix="/auth", tags=["auth"])
+api_router.include_router(profile.profile_router, prefix="/profile", tags=["profile"])
 api_router.include_router(role.role_router, prefix="/role", tags=["role"])
 api_router.include_router(department.department_router, prefix="/department", tags=["department"])
 api_router.include_router(issues.router, prefix="/issues", tags=["issues"])
