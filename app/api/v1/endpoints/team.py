dafa3eaaaf3764221a26660e6281730f5c7223d0
Parakram <parakram@keemail.me>
2026-03-12 11:52:50 +0545

    Endpoint to get Team info

--- app/api/v1/endpoints/team.py
+++ app/api/v1/endpoints/team.py
@@ -2,20 +2,42 @@
 Team endpoints.
 """
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.v1.dependencies import require_department_admin
 from app.db.session import get_db
+from app.models.employee import Employee
 from app.models.user import User
+from app.repositories.employee_repo import EmployeeRepository
 from app.repositories.team_repo import TeamRepository
-from app.repositories.user_repo import UserRepository
-from app.schemas.team import Team, TeamCreate
+from app.schemas.team import Team, TeamCreate, TeamList
 
 team_router = APIRouter()
 
 
-@team_router.get("/teams")
+@team_router.get(
+    "/list-teams",
+    dependencies=[Depends(require_department_admin)],
+    response_model=TeamList,
+    summary="List Teams of My Department",
+    description="Get all teams of my department.",
+    responses={
+        403: {
+            "description": "Access forbidden: Department Admin or Superadmin only",
+            "content": {"application/json": {"example": {"detail": "Not enough permissions"}}},
+        },
+        404: {
+            "description": "Current user doesn't have an employee profile "
+            "or is not associated with any department",
+            "content": {
+                "application/json": {
+                    "example": {"detail": "Current user does not have an employee profile."}
+                }
+            },
+        },
+    },
+)
 async def get_teams(
     db: AsyncSession = Depends(get_db),
     current_user: User = Depends(require_department_admin),
@@ -23,22 +45,26 @@ async def get_teams(
     """
     Get all teams of my department.
     """
-    user_repository = UserRepository(db)
-    user_with_profile = await user_repository.get_user_with_employee_profile(
+    employee_repo = EmployeeRepository(db)
+    employee: Employee | None = await employee_repo.get_employee_by_user_id(
         user_id=current_user.user_id
     )
-    employee_profile = user_with_profile.employee_profile if user_with_profile else None
-    if not employee_profile:
-        return {"error": "Current user does not have an employee profile."}
-    user_department = employee_profile.department_id
-    if not user_department:
-        return {"error": "Current user's employee profile is not associated with any department."}
+    if not employee:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Current user does not have an employee profile.",
+        )
 
+    user_department = employee.department_id
+
+    if not user_department:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Current user's employee profile is not associated with any department.",
+        )
     team_repository = TeamRepository(db)
-    teams: list[Team] = await team_repository.list_teams_by_department(
-        department_id=user_department
-    )
-    return {"teams": teams}
+    teams = await team_repository.list_teams_by_department(department_id=user_department)
+    return TeamList(teams=teams)
 
 
 @team_router.get("/teams/{team_id}")
@@ -50,11 +76,60 @@ async def get_team(
     """
     Get a team by ID.
     """
-    # Placeholder implementation, replace with actual database query
-    return {"message": f"Details of team with ID {team_id} will be returned here."}
+    employee_repo = EmployeeRepository(db)
+    employee: Employee | None = await employee_repo.get_employee_by_user_id(
+        user_id=current_user.user_id
+    )
+    if not employee:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Current user does not have an employee profile.",
+        )
+    user_department = employee.department_id
+    if not user_department:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Current user's employee profile is not associated with any department.",
+        )
+
+    team_repository = TeamRepository(db)
+    team: Team | None = await team_repository.get_team_by_id(team_id=team_id)
+    if not team:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Team not found.",
+        )
+    if team.department_id != user_department:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You do not have access to this team.",
+        )
+    return {"team": team}
 
 
-@team_router.post("/create-team", dependencies=[Depends(require_department_admin)])
+@team_router.post(
+    "/create-team",
+    dependencies=[Depends(require_department_admin)],
+    status_code=status.HTTP_201_CREATED,
+    summary="Create a New Team",
+    description="Create a new team for current user's department(Requires department admin)",
+    response_model=dict,
+    responses={
+        403: {
+            "description": "Access forbidden: Department Admin or Superadmin only",
+            "content": {"application/json": {"example": {"detail": "Not enough permissions"}}},
+        },
+        404: {
+            "description": "Current user doesn't have an employee profile"
+            " or is not associated with any department",
+            "content": {
+                "application/json": {
+                    "example": {"detail": "Current user does not have an employee profile."}
+                }
+            },
+        },
+    },
+)
 async def create_team(
     team: TeamCreate,
     db: AsyncSession = Depends(get_db),
@@ -63,22 +138,28 @@ async def create_team(
     """
     Create a new team.
     """
-    user_repository = UserRepository(db)
-    user_with_profile = await user_repository.get_user_with_employee_profile(
+    employee_repo = EmployeeRepository(db)
+    employee: Employee | None = await employee_repo.get_employee_by_user_id(
         user_id=current_user.user_id
     )
-    employee_profile = user_with_profile.employee_profile if user_with_profile else None
-    if not employee_profile:
-        return {"error": "Current user does not have an employee profile."}
-    user_department = employee_profile.department_id
+    if not employee:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Current user does not have an employee profile.",
+        )
+
+    user_department = employee.department_id
     if not user_department:
-        return {"error": "Current user's employee profile is not associated with any department."}
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Current user's employee profile is not associated with any department.",
+        )
 
     team_repository = TeamRepository(db)
     new_team: Team = await team_repository.create_team(team_create=team, dep_id=user_department)
     return {
-        "message": "Team created successfully. {team_name} is now available to handle issues"
-        " in department {department_id}.".format(
-            team_name=new_team.team_name, department_id=user_department
-        ),
+        "message": (
+            f"Team created successfully. {new_team.team_name} is now available to handle "
+            f"issues in department {user_department}."
+        )
     }
